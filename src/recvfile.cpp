93307d08383540f9f27e84a56ffafdbae13432db
corneliusyan <onel.raja@gmail.com>
2018-10-27 21:33:26 +0700

    RECEIVEFILE GAFASDFHJKLASDJFKLASDJFKLASDJFLKASDJKLFADSJKLF

--- src/recvfile.cpp
+++ src/recvfile.cpp
@@ -4,88 +4,142 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include "packet.cpp"
+#include "ack.cpp"
+#define H_ACK 1
+#define H_NAK 0
+#define H_PACK 1
+#define H_EOF 0
+#define MAX_BUFFER_SIZE 11000
 
-int main(int argc, char *argv[])
-{
-    int s;
-    uint32_t namelen, client_address_size;
-    unsigned short port;
-    struct sockaddr_in client, server;
-    char buf[32];
+const int MAX_PACKET_SIZE = MAX_DATA_LENGTH + 10;
 
-    /* argv[4] is internet address of server argv[5] is port of server.
-    * Convert the port from ascii to integer and then from host byte
-    * order to network byte order.
-    */
-    if (argc != 4)
-    {
-        printf("Usage: %s <filename> <windowsize> <buffersize> <port> \n", argv[0]);
-        exit(1);
-    }
-    port = htons(atoi(argv[4]));
+uint32_t nextSequenceNumber = 0;
 
-    /*
-    * Create a datagram socket in the internet domain and use the
-    * default protocol (UDP).
-    */
+bool done = false;
+int s;
+uint32_t namelen, client_address_size;
+unsigned short port;
+struct sockaddr_in client, server;
+char *buf;
+int windowsize, buffersize, maxPacketsInBuffer,idxbuf=0;
+
+FILE *f;
+
+void createSocket(){
+    
     if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("socket()");
         exit(1);
     }
+}
 
-    /*
-    * Bind my name to this socket so that clients on the network can
-    * send me messages. (This allows the operating system to demultiplex
-    * messages and get them to the correct server)
-    *
-    * Set up the server name. The internet address is specified as the
-    * wildcard INADDR_ANY so that the server can get messages from any
-    * of the physical internet connections on this host. (Otherwise we
-    * would limit the server to messages from only one network
-    * interface.)
-    */
+void setupServer(unsigned short port){
+    
     server.sin_family = AF_INET;         /* Server is in Internet Domain */
     server.sin_port = port;              /* Use this port                */
     server.sin_addr.s_addr = INADDR_ANY; /* Server's Internet Address    */
+}
 
+void bindServer(){
+    
     if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
     {
         perror("bind()");
         exit(2);
     }
+    
+}
 
-    /*
-    * Receive a message on socket s in buf  of maximum size 32
-    * from a client. Because the last two paramters
-    * are not null, the name of the client will be placed into the
-    * client data structure and the size of the client address will
-    * be placed into client_address_size.
-    */
+void dealocateSocket(){
+    close(s);
+}
+
+
+void sendACK(ACK& ack)
+{
+    if (sendto(s, ack.message, sizeof(ack), 0, (struct sockaddr *)&client, sizeof(client)) < 0)
+    {
+        perror("sendto()");
+        exit(2);
+    }
+}
+
+void sendWrongACK(){
+    ACK desiredACK(nextSequenceNumber,true);
+    sendACK(desiredACK);
+}
+
+void processMessage(){
+    printf("masuk processmessage\n");
+
     client_address_size = sizeof(client);
+    buf = new char[1034];
+    
 
-    if (recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&client, &client_address_size) < 0)
+    if (recvfrom(s,buf, 1034, 0, (struct sockaddr *)&client, &client_address_size) < 0)
     {
         perror("recvfrom()");
         exit(4);
     }
-    /*
-    * Print the message and the name of the client.
-    * The domain should be the internet domain (AF_INET).
-    * The port is received in network byte order, so we translate it to
-    * host byte order before printing it.
-    * The internet address is received as 32 bits in network byte order
-    * so we use a utility that converts it to a string printed in
-    * dotted decimal format for readability.
-    */
-    printf("Received message %s from domain %s port %d internet address %s\n",
-           buf,
-           (client.sin_family == AF_INET ? "AF_INET" : "UNKNOWN"),
-           ntohs(client.sin_port),
-           inet_ntoa(client.sin_addr));
 
-    /*
-    * Deallocate the socket.
-    */
-    close(s);
+    Packet currPacket(buf);
+    
+    if(currPacket.getSequenceNumber() != nextSequenceNumber){
+        sendWrongACK();
+        return;
+    }
+    printf("getdata :\n");
+    currPacket.printMessage();
+    
+
+    printf("tengah\n");
+    char* test = currPacket.getData();
+    printf("%d\n", currPacket.getDataLength());
+    for (int i = 0; i < currPacket.getDataLength(); i++) {
+        printf("%c\n", test[i]);
+    }
+    // printf("sebelum\n");
+    // memcpy(buffer,currPacket.getData(),currPacket.getDataLength());
+    // printf("tengah\n");
+    // buffer[currPacket.getDataLength()]='\0';
+    // printf("buffer : %s\n",buffer);
+    //fprintf(f,buffer);
+    printf("asdfasdfasdfasdfasdfasdfasdfasd\n");
+    if(currPacket.getSOH()==H_EOF){
+        done = true;
+    }
+    
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 5)
+    {
+        printf("Usage: %s <filename> <windowsize> <buffersize> <port> \n", argv[0]);
+        exit(1);
+    }
+    windowsize = atoi(argv[2]);
+    buffersize = atoi(argv[3]);
+    maxPacketsInBuffer = buffersize/MAX_PACKET_SIZE;
+    buf = new char[maxPacketsInBuffer*MAX_PACKET_SIZE];
+
+    createSocket();
+    setupServer(htons(atoi(argv[4])));
+    bindServer();
+
+    f = fopen(argv[1],"w");
+    if (f==NULL){
+        printf("Failed to write file. Exit\n");
+        exit(0);
+    }
+
+    while(!done){
+        
+        processMessage();
+    }
+    
+    
 }

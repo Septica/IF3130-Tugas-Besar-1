b9d604094e1bb82b45c52265c714bb607a0b33b5
Felix Septianus Darmawan <felix.septianus@gmail.com>
2018-10-27 12:03:30 +0700

    Fix packet

--- src/packet.cpp
+++ src/packet.cpp
@@ -16,45 +16,43 @@ class Packet {
         
         uint32_t getSequenceNumber() {
             uint32_t sequenceNumber;
-            memcpy(message + 1, &sequenceNumber, sizeof(sequenceNumber));
+            memcpy(&sequenceNumber, message + 1, sizeof(sequenceNumber));
             return sequenceNumber;
         }
 
         uint32_t getDataLength() {
             uint32_t dataLength;
-            memcpy(message + 5, &dataLength, sizeof(dataLength));
+            memcpy(&dataLength, message + 5, sizeof(dataLength));
             return dataLength;
         }
 
         char* getData() {
             char* data = new char[getDataLength()];
-            memcpy(message + 9, &data, getDataLength());
+            memcpy(&data, message + 9, getDataLength());
             return data;
         }
 
         char getChecksum() {
             uint32_t checksum;
-            memcpy(message + 9 + getDataLength(), &checksum, sizeof(checksum));
+            memcpy(&checksum, message + 9 + getDataLength(), sizeof(checksum));
             return checksum;
         }
 
         void printMessage() {
-            for( int i = 0; i < 10 + getDataLength(); i++) {
-                printf("%x", message[i]);
+            for( int i = 0; i < 10 + 1; i++) {
+                printf("Byte %d: %x\n", i, message[i]);
             }
-            printf("\n");
         }
 
         const char SOH = 0x1;
         
-
     private:
         static uint32_t nextSequenceNumber;
         char* message;
 
         char calculateChecksum() {
             char sum = 0;
-            for (int i = 0; i < 9; i++) {
+            for (int i = 0; i < 10 + getDataLength(); i++) {
                 sum += message[i];
             }
             return sum;
